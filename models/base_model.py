97c02aed2a4a766b5893277fa8904a88e1cd8180
Hexdee <lukmansaheed79@gmail.com>
2023-10-16 04:54:34 +0100

    Create AirBnb console

--- models/base_model.py
+++ models/base_model.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
-"""
-BaseModel class: This class serves as the
-base class for all our models in the application.
-"""
-
-from uuid import uuid4
+"""This module defines a base class for all models in our hbnb clone"""
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all related classes"""
-
+    """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """
-        __init__: Constructor method that either deserializes
-        a serialized class or initializes a new instance.
-        """
-        # Initialize if no keyword arguments are passed
-        if kwargs == {}:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            return
+        """Instatntiates a new model"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """
-        __str__: Override the string representation of self.
-        """
-        str_fmt = f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
-        return str_fmt
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        """
-        save: Updates the last updated variable and saves to storage.
-        """
-        self.updated_at = datetime.utcnow()
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """
-        to_dict: Returns a dictionary representation of self.
-        """
-        dic = {**self.__dict__}
-        dic['__class__'] = type(self).__name__
-        dic['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        dic['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return dic
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
